91ce266705fb1cfde625707f1a6bc876f2aa6ce7
Cory Nelson <conelso@microsoft.com>
2019-11-22 15:17:47 -0800

    WIP

--- src/libraries/System.Net.Http/src/System/Net/Http/Headers/AltSvcHeaderParser.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/Headers/AltSvcHeaderParser.cs
@@ -28,14 +28,15 @@ namespace System.Net.Http.Headers
                 return 0;
             }
 
-            int alpnProtocolNameLength = HttpRuleParser.GetTokenLength(value, startIndex);
+            int idx = startIndex;
 
-            if (alpnProtocolNameLength == 0)
+            int alpnProtocolNameLength = HttpRuleParser.GetTokenLength(value, idx);
+            string alpnProtocolName = ReadPercentEncodedAlpnProtocolName(value.AsSpan(idx, alpnProtocolNameLength));
+            if (alpnProtocolName == null)
             {
-                throw new Exception("Invalid ALPN protocol name: must not be empty.");
+                throw new Exception("Unknown ALPN protocol name.");
             }
-
-            string alpnProtocolName = ReadPercentEncodedAlpnProtocolName(value.AsSpan(startIndex, alpnProtocolNameLength));
+            idx += alpnProtocolNameLength;
 
             // Clear should be used alone with no parameters.
             if (ReferenceEquals(alpnProtocolName, "clear"))
@@ -44,21 +45,54 @@ namespace System.Net.Http.Headers
                 ret.Alternatives.Add(AlternateService.Clear);
 
                 parsedValue = ret;
-                return alpnProtocolNameLength;
+                return idx - startIndex;
             }
 
-            int pos = startIndex + alpnProtocolNameLength;
-
-            if (value[pos] != '=')
+            if (idx == value.Length || value[idx++] != '=')
             {
                 throw new Exception("Invalid alternative, expected '='.");
             }
 
-            // alt-authority as a quoted string.
-            if (value[++pos] != '"')
+            if (!TryReadAltAuthority(value, idx, out string altAuthorityHost, out int altAuthorityPort, out int altAuthorityLength))
             {
-                int 
+                throw new Exception("Invalid alt-authority, excepected quoted string.");
             }
+            idx += altAuthorityLength;
+
+            // Parse parameters.
+            int? maxAge = null;
+
+            while (idx != value.Length)
+            {
+                // Skip OWS.
+                while (idx != value.Length && IsOptionalWhiteSpace(value[idx])) ++idx;
+
+                // get the parameter key length.
+                int tokenLength = HttpRuleParser.GetTokenLength(value, idx);
+                if (tokenLength == 0)
+                {
+                    break;
+                }
+
+                if (tokenLength + 1 != '=')
+                {
+                    throw new Exception("Expected = in parameter.");
+                }
+
+                if (tokenLength == 2 && value[idx] == 'm' && value[idx + 1] == 'a')
+                {
+                    maxAge = 
+                }
+
+                int parameterIdx = idx;
+
+                if()
+            }
+        }
+
+        private static bool IsOptionalWhiteSpace(char ch)
+        {
+            return ch == ' ' || ch == '\t';
         }
 
         private static string ReadPercentEncodedAlpnProtocolName(ReadOnlySpan<char> span)
@@ -83,10 +117,10 @@ namespace System.Net.Http.Headers
                     if (span.SequenceEqual("clear")) return "clear";
                     goto default;
                 case 10:
-                    if (span.StartsWith("http%2") && span[8] == '.')
+                    if (span.StartsWith("http%2") && span[7] == '1' && span[8] == '.')
                     {
                         char ch = span[6];
-                        if ((ch == 'F' || ch == 'f') && span[7] == '1')
+                        if (ch == 'F' || ch == 'f')
                         {
                             ch = span[9];
                             if (ch == '1') return "http/1.1";
@@ -102,8 +136,126 @@ namespace System.Net.Http.Headers
             }
         }
 
-        private static bool TryParseQuotedString(string value, int startIndex, out string readValue, out int readLength)
+        private static bool TryReadAltAuthority(string value, int startIndex, out string host, out int port, out int readLength)
         {
+            if (HttpRuleParser.GetQuotedStringLength(value, startIndex, out int quotedLength) == HttpParseResult.Parsed)
+            {
+                goto parseError;
+            }
+
+            Debug.Assert(value[startIndex] == '"' && value[startIndex + quotedLength - 1] == '"', $"{nameof(HttpRuleParser.GetQuotedStringLength)} should return {nameof(HttpParseResult.NotParsed)} if the opening/closing quotes are missing.");
+            ReadOnlySpan<char> quoted = value.AsSpan(startIndex + 1, quotedLength - 2);
+
+            int idx = quoted.IndexOf(':');
+            if (idx == -1)
+            {
+                goto parseError;
+            }
+
+            // Parse out the port.
+            if (!TryUnquotePort(quoted.Slice(idx + 1), out port))
+            {
+                goto parseError;
+            }
+
+            // Parse out the optional host.
+            if (--idx == 0)
+            {
+                host = null;
+            }
+            else if (!TryReadQuotedValue(quoted.Slice(0, idx), out host))
+            {
+                goto parseError;
+            }
+
+            readLength = quotedLength;
+            return true;
+
+        parseError:
+            host = null;
+            port = 0;
+            readLength = 0;
+            return false;
         }
+
+        private static bool TryReadQuotedValue(ReadOnlySpan<char> value, out string result)
+        {
+            int idx = value.IndexOf('\\');
+
+            if (idx == -1)
+            {
+                // Hostnames shouldn't require quoted pairs, so this should be the hot path.
+                result = value.Length != 0 ? new string(value) : null;
+                return true;
+            }
+
+            var builder = new ValueStringBuilder(stackalloc char[128]);
+
+            do
+            {
+                if (idx + 1 == value.Length)
+                {
+                    // quoted-pair requires two characters: the quote, and the quoted character.
+                    builder.Dispose();
+                    result = null;
+                    return false;
+                }
+
+                if (idx != 0)
+                {
+                    builder.Append(value.Slice(0, idx));
+                }
+
+                builder.Append(value[idx + 1]);
+
+                value = value.Slice(idx + 2);
+                idx = value.IndexOf('\\');
+            }
+            while (idx != -1);
+
+            if (value.Length != 0)
+            {
+                builder.Append(value);
+            }
+
+            result = builder.ToString();
+            return true;
+        }
+
+        private static bool TryReadQuotedInt32(ReadOnlySpan<char> value, out int result)
+        {
+            if (value.Length == 0)
+            {
+                result = 0;
+                return false;
+            }
+
+            int port = 0;
+
+            try
+            {
+                foreach (char ch in value)
+                {
+                    // The port shouldn't ever need a quoted-pair, but they're still valid... skip if found.
+                    if (ch == '\\') continue;
+
+                    if (ch < '0' || ch > '9')
+                    {
+                        result = 0;
+                        return false;
+                    }
+
+                    checked
+                    {
+                        port = port * 10 + (ch - '0');
+                    }
+                }
+            }
+
+            result = port;
+            return true;
+        }
+
+        private static bool TryReadTokenOrQuotedString(string value, int startIndex, out string)
     }
 }
